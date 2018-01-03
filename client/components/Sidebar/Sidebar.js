1f074ea8dbe794e18fde9fa32f346db03f388393
borenwu <borenwu@163.com>
2018-01-03 23:26:38 +0800

    second

--- client/components/Sidebar/Sidebar.js
+++ client/components/Sidebar/Sidebar.js
@@ -74,26 +74,26 @@ class Sidebar extends Component {
               <NavLink to={'/charts'} className="nav-link" activeClassName="active"><i className="icon-pie-chart"></i> Charts</NavLink>
             </li>
             <li className="divider"></li>
-            <li className="nav-title">
-              Extras
-            </li>
-            <li className="nav-item nav-dropdown">
-              <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-star"></i> Pages</a>
-              <ul className="nav-dropdown-items">
-                <li className="nav-item">
-                  <NavLink to={'/login'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Login</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/register'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Register</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/404'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Error 404</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to={'/500'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Error 500</NavLink>
-                </li>
-              </ul>
-            </li>
+            {/*<li className="nav-title">*/}
+              {/*Extras*/}
+            {/*</li>*/}
+            {/*<li className="nav-item nav-dropdown">*/}
+              {/*<a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-star"></i> Pages</a>*/}
+              {/*<ul className="nav-dropdown-items">*/}
+                {/*<li className="nav-item">*/}
+                  {/*<NavLink to={'/login'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Login</NavLink>*/}
+                {/*</li>*/}
+                {/*<li className="nav-item">*/}
+                  {/*<NavLink to={'/register'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Register</NavLink>*/}
+                {/*</li>*/}
+                {/*<li className="nav-item">*/}
+                  {/*<NavLink to={'/404'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Error 404</NavLink>*/}
+                {/*</li>*/}
+                {/*<li className="nav-item">*/}
+                  {/*<NavLink to={'/500'} className="nav-link" activeClassName="active"><i className="icon-star"></i> Error 500</NavLink>*/}
+                {/*</li>*/}
+              {/*</ul>*/}
+            {/*</li>*/}
           </ul>
         </nav>
       </div>
